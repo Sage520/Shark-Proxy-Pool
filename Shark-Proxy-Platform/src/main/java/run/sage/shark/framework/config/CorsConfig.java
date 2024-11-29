d94471fd6bc1cdd797a0a4bd1bc0bf647320c8a3
Sage <sagelovemio@qq.com>
2024-11-29 17:08:32 +0800

    feat: 接口增加跨域配置

---
+++ Shark-Proxy-Platform/src/main/java/run/sage/shark/framework/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package run.sage.shark.framework.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 跨域配置
+ * @author Sage
+ * @date 2024/11/29
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
