e1e2b0b4a75caf12aa3e7e620bf8dec9536aed83
Sage <sagelovemio@qq.com>
2024-11-29 16:16:54 +0800

    feat: 优化配置文件

--- Shark-Proxy-Spider/test.py
+++ Shark-Proxy-Spider/test.py
@@ -4,5 +4,5 @@ from mq.producer import send_to_add_queue
 from fetcher.fetcher import ProxyFetcher
 
 if __name__ == '__main__':
-    for i in ProxyFetcher.ip3366():
+    for i in ProxyFetcher.common():
         send_to_add_queue(i)
